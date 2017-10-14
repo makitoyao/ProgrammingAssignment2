29d9210f5960d16ce407cb8adb3c8733c06e00ba
makitoyao <makitoyao@gmail.com>
2017-10-14 16:33:52 +0900

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+
+# This function creates a special "matrix" object that can cache its inverse
+# first creat a variable call x as x<-makeCacheMatrix()
+# set: set a matrix to x
+# get: get matrix from x
+# setInverse: set inverse matrix to list x
+# getInverse: get inverse matrix from x, if exsists
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse_x <- NULL
+        
+        # set matrix to variable x
+        set <- function(y) {
+                if (class(y) == 'matrix'){
+                        x <<- y
+                        inverse_x <<- NULL
+                }
+                else{
+                        print('Input is not a matrix')
+                }
+        }
+        
+        # get matrix from variable x
+        get <- function() {
+                x
+        }
+        
+        # set inverse matrix 
+        setInverse <- function(inverseToSet) {
+                inverse_x <<- inverseToSet
+        }
+        
+        # get inverse matrix
+        getInverse <- function() {
+                inverse_x
+        }
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+
+# This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated 
+# (and the matrix has not changed), then cacheSolve should retrieve the 
+# inverse from the cache.
 cacheSolve <- function(x, ...) {
+        inverse_x <- x$getInverse()
+        if(!is.null(inverse_x)) {
+                message("getting cached data")
+                return(inverse_x)
+        }
+        data <- x$get()
+        inverse_x <- solve(data, ...)
+        x$setInverse(inverse_x)
+        inverse_x
         ## Return a matrix that is the inverse of 'x'
 }
